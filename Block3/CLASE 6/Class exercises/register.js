2ae41c981d478b2bcd221a0d35cbb8c1681df7fa
caroMaio <carolinabelenmaio@gmail.com>
2020-11-20 12:40:11 -0300

    HIDEFORM() and SHOWUSER() out of FOR OF, in register function

--- Block3/CLASE 6/Class exercises/register.js
+++ Block3/CLASE 6/Class exercises/register.js
@@ -1,41 +1,39 @@
 const ids = ["name", "lastname", "email", "password"];
 let user = {};
 
-
 const showPassword = () => {
-    const password = document.getElementById("password");
+  const password = document.getElementById("password");
   password.type === "password"
-  ? (password.type = "text")
-  : (password.type = "password");
+    ? (password.type = "text")
+    : (password.type = "password");
 };
 
 const hideForm = () => {
-    const form = document.getElementById("form-container");
-    form.classList.add('hidden');
-}
+  const form = document.getElementById("form-container");
+  form.classList.add("hidden");
+};
 
 const showUser = () => {
-    const userBox = document.getElementById("user-container");
-    userBox.classList.add('show')
-    const title = document.getElementById("welcome-title");
-    const emailUser = document.getElementById("email-user")
-    title.innerText = user.name || 'Bienvenido Usuario';
-    emailUser.innerHTML = user.email;
-}
-
+  const userBox = document.getElementById("user-container");
+  userBox.classList.add("show");
+  const title = document.getElementById("welcome-title");
+  const emailUser = document.getElementById("email-user");
+  title.innerText = user.name || "Bienvenido Usuario";
+  emailUser.innerHTML = user.email;
+};
 
 const register = () => {
-    for (id of ids) {
-        const element = document.getElementById(id).value;
-        user[id] = element;
-        hideForm();
-        showUser();
-    }
+  for (id of ids) {
+    const element = document.getElementById(id).value;
+    user[id] = element;
+  }
+  hideForm();
+  showUser();
 };
 
 const goBack = () => {
-    const userBox = document.getElementById("user-container");
-    userBox.classList.remove('show');
-    const form = document.getElementById("form-container");
-    form.classList.remove('hidden');
-}
+  const userBox = document.getElementById("user-container");
+  userBox.classList.remove("show");
+  const form = document.getElementById("form-container");
+  form.classList.remove("hidden");
+};
